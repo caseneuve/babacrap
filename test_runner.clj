c1e61679e5a5bea32bd407ddb3d17a54436eaa70
Piotr Kaznowski <piotr@kazno.dev>
2026-04-26 11:44:57 +0200

    [test] simplify babashka test runner.

--- test_runner.clj
+++ test_runner.clj
@@ -1,12 +1,14 @@
 #!/usr/bin/env bb
 
 (ns test-runner
-  (:require [babacrap.detangle-test]
-            [babacrap.integration-test]
-            [clojure.test :as test]))
+  (:require [clojure.test :as test]))
 
-(let [{:keys [fail error]} (test/run-tests 'babacrap.integration-test
-                                           'babacrap.detangle-test)]
-  (when (pos? (+ fail error))
-    (System/exit 1))
-  (println "ok"))
+(def test-namespaces
+  '[babacrap.integration-test
+    babacrap.detangle-test])
+
+(doseq [ns-sym test-namespaces]
+  (require ns-sym))
+
+(let [{:keys [fail error]} (apply test/run-tests test-namespaces)]
+  (System/exit (+ fail error)))
